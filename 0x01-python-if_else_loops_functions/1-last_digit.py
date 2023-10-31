4aa84315fee5c566e02f938d8209a9f90c470eed
BlessKobby <amablebless@gmail.com>
2023-10-31 06:12:40 -0700

    Conditionals_iteratives

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,8 @@ elif number < 0:
         if 10 - (number % 10) != 0:
             print(f"Last digit of {number} is {10 - number % 10}\
  and is less than 6 and not 0")
+elif number % 10 == 0:
+    print(f"Last digits of {number} is {number % 0} and is 0")
 else:
     print(f"Last digit of {number} is {10 - number % 10}\
  and is greater than 5")

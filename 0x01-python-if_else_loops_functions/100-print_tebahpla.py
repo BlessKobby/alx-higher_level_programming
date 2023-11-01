d4b9a483670947f8af57dcbc715bcc7842e08457
BlessKobby <amablebless@gmail.com>
2023-11-01 10:09:33 -0700

    Conditionals_iteratives

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 for i in range(ord('z'), ord('a') - 1, -1):
     char = chr(i)
     if i % 2 == 1:
-        char = char.upper()
-    print(char, end='')
+        char = chr(i - ord('a') + ord('A'))
+    print("{}".format(char), end='')

2c5920821e76a2c7976df0030eea8791e3c55424
BlessKobby <amablebless@gmail.com>
2023-11-02 08:26:05 -0700

    Python_import_modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,11 +2,16 @@
 
 # A Python script that works exactly as a given byte-code. #
 
+from magic_calculation_102 import add, sub
+
 def magic_calculation(a, b):
+    add_func = add
+    sub_func = sub
+
     if a < b:
-        c = add(a, b)
+        c = add_func(a, b)
         for i in range(4, 6):
-            c = add(c, i)
-            return c
-        else:
-            return sub(a, b)
+            c = add_func(c, i)
+        return c
+    else:
+        return sub_func(a, b)

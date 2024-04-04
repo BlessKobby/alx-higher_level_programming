8f948afcc44cd5b3ab32025eff077fbff772397e
BlessKobby <amablebless@gmail.com>
2024-04-04 03:30:44 -0700

    JavaScript - Web scraping.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script to write a contetn to a file.
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
